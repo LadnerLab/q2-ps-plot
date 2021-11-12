9a670633071f70f62eb392abe4872f450aae5c18
Annabelle Brown <amb2528@nau.edu>
2021-11-12 10:52:42 -0700

    add plotlog and fix resolution of png

--- q2_ps_plot/actions/boxplot.py
+++ q2_ps_plot/actions/boxplot.py
@@ -9,11 +9,12 @@ def readCountsBoxplot(
     output_dir: str,
     read_counts: pd.DataFrame) -> None:
     
-    # read_counts['all samples'] = read_counts['Sum of probe scores'] / 1000000
-
+    #collects min and max values of sum of probes
     rc_min = min(read_counts['Sum of probe scores'])
     rc_max = max(read_counts['Sum of probe scores'])
 
+
+    #creates boxplot
     chart = alt.Chart(read_counts).transform_fold(
         ['Sum of probe scores'],
         as_=['key', 'value']
@@ -22,5 +23,12 @@ def readCountsBoxplot(
         y=alt.Y('value:Q', axis = alt.Axis(title="Value (1e6)"), scale=alt.Scale(domain=[rc_min, rc_max]))
     ).properties(width=300)
 
-    save(chart, "readCountBoxplot.png")
+    # saves boxplot as a png file and to index.html for creation of qzv file
+    save(chart, "readCountBoxplot.png", scale_factor=10)
     chart.save(os.path.join(output_dir, "index.html"))
+
+def enrichmentBoxplot(
+    output_dir = str,
+    enriched_dir = pd.DataFrame) -> None:
+
+    pass
--- q2_ps_plot/actions/scatter.py
+++ q2_ps_plot/actions/scatter.py
@@ -4,6 +4,8 @@ import numpy as np
 from collections import defaultdict
 import qiime2, itertools, os
 
+
+# function to collect pairwise pairs of samples
 def _make_pairs_list(column):
     series = column.to_series()
     pairs = {k: v.index for k,v in series.groupby(series)}
@@ -12,27 +14,46 @@ def _make_pairs_list(column):
         result.append(list(itertools.combinations(ids, 2)))
     return result
 
-def repZscatters(
+def repScatters(
     output_dir: str,
-    zscore: pd.DataFrame,
-    source: qiime2.CategoricalMetadataColumn = None)->None:
+    source: qiime2.CategoricalMetadataColumn,
+    plot_log: bool = False,
+    zscore: pd.DataFrame = None,
+    col_sum: pd.DataFrame = None)->None:
 
-    zscore = zscore.transpose()
+    # check wether zscore matrix or colsum matrix was provided
+    if zscore is not None:
+        data = zscore.transpose()
+    if col_sum is not None:
+        data = col_sum.transpose()
 
+    # start the default dict and add the following column names or keys
     hmDic = defaultdict(list)
     columnNms = ['sample', 'bin_x_start', 'bin_x_end', 'bin_y_start', 'bin_y_end', 'count']
     for nm in columnNms:
         hmDic[nm]
 
+    # call function to collect samples pairs
     pairs = _make_pairs_list(source)
+
+    # initialize empty list to collet sample names for dropdown menu
     samples = []
+
+    # start loop to collect heatmap values
     for lst in pairs:
         for tpl in lst:
             samp = '~'.join(tpl)
             samples.append(samp)
-            x = list(zscore[tpl[0]])
-            y = list(zscore[tpl[1]])
 
+            #set x and y values from the dataframe
+            x = list(data[tpl[0]])
+            y = list(data[tpl[1]])
+
+            if plot_log:
+                x = np.log10(np.array(x)+1)
+                y = np.log10(np.array(y)+1)
+
+            #generate heatmap values
             heatmap, xedges, yedges = np.histogram2d(x, y, bins=(70,70))
             for x in range(0, heatmap.shape[0]):
                 for y in range(0, heatmap.shape[1]):
@@ -45,6 +66,7 @@ def repZscatters(
                     bin_y_start = yedges[y]
                     bin_y_end = yedges[y+1]
 
+                    #place heatmap values in the dictionary created
                     hmDic['sample'].append(samp)
                     hmDic['bin_x_start'].append(bin_x_start)
                     hmDic['bin_x_end'].append(bin_x_end)
@@ -52,11 +74,14 @@ def repZscatters(
                     hmDic['bin_y_end'].append(bin_y_end)
                     hmDic['count'].append(count)
     
+    #convert the heatmap to a dataframe
     hmDf = pd.DataFrame(hmDic)
 
+    # set the dropdown values
     sample_dropdown = alt.binding_select(options=samples, name='Sample Select')
     sample_select = alt.selection_single(fields=['sample'], bind=sample_dropdown, name="sample", init={'sample': samples[0]})
 
+    # create scatterplot chart with attached dropdown menu
     heatmapChart = alt.Chart(hmDf).mark_rect().encode(
             alt.X('bin_x_start:Q', title="Sample1"),
             alt.X2('bin_x_end:Q'),
@@ -69,6 +94,7 @@ def repZscatters(
         sample_select
     )
 
+    # save the chart to index.html to be saved to a .qzv file
     heatmapChart.save(os.path.join(output_dir, "index.html"))
 
 
