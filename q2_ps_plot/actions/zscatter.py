7c504f3dc1c63c64bcda7d98614c483bb8bf9c8f
Num0Programmer <asajh4@gmail.com>
2024-02-29 11:42:35 -0700

    Implemented drop down for spline chart. No luck combining everything into one chart yet.

--- q2_ps_plot/actions/zscatter.py
+++ q2_ps_plot/actions/zscatter.py
@@ -89,111 +89,119 @@ def zscatter(
     final_chart = alt.layer(heatmap_chart)
    
 
-    if species_taxa_file and highlight_thresholds:
-        samples = zscores.columns.to_list()
-        highlight_dict = {
-            "x": list(), "y": list(),
-            "tooltip": list(), "highlight": list(), "pair": list()
-        }
+    # if species_taxa_file and highlight_thresholds:
+    #     samples = zscores.columns.to_list()
+    #     highlight_dict = {
+    #         "x": list(), "y": list(),
+    #         "tooltip": list(), "highlight": list(), "pair": list()
+    #     }
 
-        with open(species_taxa_file, "r") as fh:
-            species_taxa = [
-                tuple(line.replace("\n", "").split("\t"))
-                for line in fh.readlines()
-            ]
+    #     with open(species_taxa_file, "r") as fh:
+    #         species_taxa = [
+    #             tuple(line.replace("\n", "").split("\t"))
+    #             for line in fh.readlines()
+    #         ]
 
-        if not os.path.isfile(highlight_data):
-            files = os.listdir(highlight_data)
-            path = highlight_data
-        else:
-            files = highlight_data
-            path = ""
+    #     if not os.path.isfile(highlight_data):
+    #         files = os.listdir(highlight_data)
+    #         path = highlight_data
+    #     else:
+    #         files = highlight_data
+    #         path = ""
 
-        print(f"Received files:\n{files}")
-        h_thresh_idx = 0
-        for file in files:
-            if "~" not in file:
-                continue
-            # TODO: will need to make this more general for other tables
-            # 1) parameters?
-            highlight_df = pd.read_csv(
-                f"{path}/{file}", sep="\t"
-            ).iloc[:, [3, 4, 8]]
-            p_vals = highlight_df.iloc[:, 0].to_list()
+    #     h_thresh_idx = 0
+    #     for file in files:
+    #         if "~" not in file:
+    #             continue
+    #         # TODO: will need to make this more general for other tables
+    #         # 1) parameters?
+    #         highlight_df = pd.read_csv(
+    #             f"{path}/{file}", sep="\t"
+    #         ).iloc[:, [3, 4, 8]]
+    #         p_vals = highlight_df.iloc[:, 0].to_list()
 
-            print(f"len(p_vals) = {len(p_vals)}\nlen(highlight_thresholds) = {len(highlight_thresholds)}\n")
-            x = []
-            y = []
-            le_peps = []
-            sig_taxas = []
-            for i in range(len(p_vals)):
-                if p_vals[i] < highlight_thresholds[h_thresh_idx]:
-                    sig_taxas.append(highlight_df.iloc[i, 2])
-                    curr_le_peps = highlight_df.iloc[i, 1].split("/")
-                    print(f"curr_le_peps: {curr_le_peps}")
-                    print(f"x = {zscores.loc[curr_le_peps, samples[0]]}")
-                    print(f"y = {zscores.loc[curr_le_peps, samples[1]]}")
-                    x.append(zscores.loc[curr_le_peps, samples[0]].to_list())
-                    y.append(zscores.loc[curr_le_peps, samples[1]].to_list())
-                    le_peps.append(curr_le_peps)
-            highlight_dict["x"].append(x)
-            highlight_dict["y"].append(y)
-            highlight_dict["tooltip"].append(le_peps)
-            highlight_dict["highlight"].append(sig_taxas)
-            h_thresh_idx += 1
+    #         x = []
+    #         y = []
+    #         le_peps = []
+    #         sig_taxas = []
+    #         for i in range(len(p_vals)):
+    #             if p_vals[i] < highlight_thresholds[h_thresh_idx]:
+    #                 sig_taxas.append(highlight_df.iloc[i, 2])
+    #                 curr_le_peps = highlight_df.iloc[i, 1].split("/")
+    #                 x.append(zscores.loc[curr_le_peps, samples[0]].to_list())
+    #                 y.append(zscores.loc[curr_le_peps, samples[1]].to_list())
+    #                 le_peps.append(curr_le_peps)
+    #         highlight_dict["x"].append(x)
+    #         highlight_dict["y"].append(y)
+    #         highlight_dict["tooltip"].append(le_peps)
+    #         highlight_dict["highlight"].append(sig_taxas)
+    #         h_thresh_idx += 1
 
-        for pair in pairs_sample_select:
-            highlight_dict["pair"].append(f"{pair}")
-        for k, v in highlight_dict.items():
-            print(f"{k}:")
-            for i in v:
-                print(f"\t(type: {type(i)}) = {i}")
-        highlight_df = pd.DataFrame(highlight_dict)
-        highlight_chart = alt.Chart(highlight_df).mark_point(
-            filled=True, size=60
-        ).encode(
-            x=alt.X("x:Q"),
-            y=alt.Y("y:Q"),
-            color=alt.Color(
-                "highlight:N",
-                scale=alt.Scale(range=[
-                    "#E69F00", "#56B4E9", "#009E73",
-                    "#F0E442", "#0072B2", "#D55E00",
-                    "#CC79A7"
-                ]),
-                legend=alt.Legend(title="Significant Taxa")
-            ),
-            # https://github.com/altair-viz/altair/issues/1181
-            # shape=alt.Shape(
-            #     "highlight:N",
-            #     legend=None
-            # ),
-            tooltip="tooltip"
-        ).transform_filter(pairs_select)
-        final_chart = alt.layer(
-            final_chart,
-            highlight_chart
-        ).resolve_scale(
-            color="independent",
-            shape="independent"
-        )
-
-
-    # if spline_file:
-    #     spline_df = pd.read_csv(spline_file, sep="\t")
-    #     spline_chart = alt.Chart(
-    #         pd.DataFrame(spline_df)
-    #     ).mark_square(size=20).encode(
-    #         x="x:Q",
-    #         y="y:Q",
+    #     for pair in pairs_sample_select:
+    #         highlight_dict["pair"].append(f"{pair}")
+    #     for k, v in highlight_dict.items():
+    #         print(f"{k}:")
+    #         for i in v:
+    #             print(f"\t(type: {type(i)}) = {i}")
+    #     highlight_df = pd.DataFrame(highlight_dict)
+    #     highlight_df.to_csv("highlight.tsv", sep="\t", index=False)
+    #     highlight_chart = alt.Chart(highlight_df).mark_point(
+    #         filled=True, size=60
+    #     ).encode(
+    #         x=alt.X("x:Q", title=None),
+    #         y=alt.Y("y:Q", title=None),
     #         color=alt.Color(
-    #             "x:N",
-    #             scale=alt.Scale(range=["#FF0000"]),
-    #             # reference: https://github.com/altair-viz/altair/issues/620
+    #             "highlight:N",
+    #             scale=alt.Scale(range=[
+    #                 "#E69F00", "#56B4E9", "#009E73",
+    #                 "#F0E442", "#0072B2", "#D55E00",
+    #                 "#CC79A7"
+    #             ]),
+    #             legend=alt.Legend(title="Significant Taxa")
+    #         ),
+    #         # https://github.com/altair-viz/altair/issues/1181
+    #         shape=alt.Shape(
+    #             "highlight:N",
     #             legend=None
-    #         )
+    #         ),
+    #         tooltip="tooltip"
+    #     ).transform_filter(pairs_select)
+    #     final_chart = alt.layer(
+    #         final_chart,
+    #         highlight_chart
+    #     ).resolve_scale(
+    #         color="independent",
+    #         shape="independent"
     #     )
-    #     final_chart = alt.layer(final_chart, spline_chart)
+
+
+    if spline_file:
+        spline_data_df = pd.read_csv(spline_file, sep="\t")
+        spline_dict = { "x": list(), "y": list(), "pair": list() }
+
+        for pair in pairs:
+            spline_dict["x"].append(spline_data_df.loc[:, pair[0]].to_list())
+            spline_dict["y"].append(spline_data_df.loc[:, pair[1]].to_list())
+            spline_dict["pair"].append(f"{pair[0]}~{pair[1]}")
+        
+        spline_df = pd.DataFrame(spline_dict)
+        print(f"Spline DataFrame:\n{spline_df}")
+        spline_chart = alt.Chart(
+            pd.DataFrame(spline_df)
+        ).mark_square(size=20).encode(
+            x="x:Q",
+            y="y:Q",
+            color=alt.Color(
+                "x:N",
+                scale=alt.Scale(range=["#FF0000"]),
+                # reference: https://github.com/altair-viz/altair/issues/620
+                legend=None
+            )
+        ).add_params(pairs_select).transform_filter(pairs_select)
+        final_chart = alt.layer(final_chart, spline_chart)
+        # final_chart = alt.layer(
+        #     final_chart, spline_chart
+        # ).add_params(pairs_select).transform_filter(pairs_select)
 
     final_chart.save(os.path.join(output_dir, "index.html"))
 
